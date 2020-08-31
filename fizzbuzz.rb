338dfd83bce635f5f171ec34ef8608d572d105c4
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-08-31 22:50:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,12 @@
 
 
 def fizzbuzz(number)
- if number % 3 == 0
-  "Fizz"
+ if number % 3 == 0 && number % 5 == 0
+  "FizzBuzz"
   elsif number % 5 == 0
   "Buzz"
- elsif number % 3 == 0 && number % 5 == 0
-  "FizzBuzz"
+ elsif number % 3 == 0 
+  "Fizz"
  end
 end
  
